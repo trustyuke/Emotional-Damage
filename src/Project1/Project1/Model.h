080cc8c641cff55dcb3502a73999ee2964d2f27e
chengzez <94012925+chengzez@users.noreply.github.com>
2022-12-11 22:28:39 -0500

    Add files via upload

---
+++ src/Project1/Project1/Model.h
@@ -0,0 +1,48 @@
+#ifndef MODEL_H
+#define MODEL_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <graphics.h>
+#include "Peg.h"
+#include <iostream>
+
+using namespace std; 
+
+class Model{
+
+public:
+    int board_size; 
+    int corner_size; 
+    int peg_total; 
+    int x_in, y_in; 
+    int x_dest,y_dest; 
+    int x_eaten,y_eaten; 
+    int STYLE;
+    Peg *peg_ptrs[70];
+    // parameter constructor 
+    Model(int style);
+
+    // find three coordinate of peg 
+    void Find3coord(int x,int y,char dir_in);
+
+    // check if the move is valid 
+    bool IsMoveValid();
+
+    // update the move and check to see if game could be continued 
+    void Update();
+
+    // display the board in UNIX
+    void displayBoard();
+
+    // display in UI
+    void refreshPage(int WIN_W, int WIN_H, int PEGW, int PEGH, IMAGE& imgBG, IMAGE* imgPEG);
+
+    bool Loadinput();
+    bool Loaddest();
+    void loadeaten();
+
+    // check to see if game could be continued (if not, print # of peg left)
+    bool IsGameOver(); 
+};
+
+#endif 
---
+++ src/Project1/Project1/Peg.h
@@ -0,0 +1,27 @@
+#ifndef PEG_H
+#define PEG_H
+
+#include <string>
+
+using namespace std; 
+
+enum PegStates {
+    EMPTY = 0,
+    OCCUPIED = 1,
+    SELECTED = 2
+}; 
+
+class Peg{
+
+public:
+    int x,y; 
+    char state; 
+
+    // parameter constructor
+    Peg(int x_loc, int y_loc,int style);
+
+
+
+}; 
+
+#endif 
